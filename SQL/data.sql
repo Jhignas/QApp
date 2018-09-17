a41e750ebe8b62955206a4e2426575e15b8ce72c
Jhignas <jhignasbhogadi@gmail.com>
2018-09-17 17:48:58 +0530

    On branch master  Changes to be committed: 	new file:   SQL/data.sql 	new file:   SQL/schema.sql
    Adding the schema and data sql files

---
+++ SQL/data.sql
@@ -0,0 +1,6 @@
+insert into questions (q_text,q_tags) values ('Is this my first question?','question,tag');
+insert into questions (q_text,q_tags) values ('Is this my second question?','question,tag');
+insert into questions (q_text,q_tags) values ('Is this my third question?','question,tag');
+insert into questions (q_text,q_tags) values ('Is this my fourth question?','question,tag');
+insert into questions (q_text,q_tags) values ('Is this my fifth question?','question,tag');
+	
