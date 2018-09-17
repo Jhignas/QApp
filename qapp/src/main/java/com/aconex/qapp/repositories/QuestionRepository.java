94973dd6afd6c80978f254f59aa3bfa73276627a
Jhignas <jhignasbhogadi@gmail.com>
2018-09-17 18:09:18 +0530

    Bringing src out into the root folder along with build.gradle file

--- qapp/src/main/java/com/aconex/qapp/repositories/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aconex.qapp.repositories;
-
-import com.aconex.qapp.entities.Question;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuestionRepository extends CrudRepository<Question,Integer> {
-}
